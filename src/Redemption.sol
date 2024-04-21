42aa4f0f958a71b3321d3f93af1a1156a52bee5d
Alex Towle <jalextowle@gmail.com>
2024-04-21 13:04:43 -0500

    Converted string reverts to custom errors

--- src/Redemption.sol
+++ src/Redemption.sol
@@ -11,14 +11,16 @@ import {IHashesDAO} from "./interfaces/IHashesDAO.sol";
 ///         redeemed for the funded ether before a deadline.
 contract Redemption is ReentrancyGuard {
     /// @notice The cap for the DEX Labs Hashes.
-    uint256 public constant DEX_LABS_CAP = 99;
+    uint256 public constant DEX_LABS_CAP = 100;
 
-    /// @notice The Hashes contract used by this redemption contract.
-    IHashes public immutable hashes;
+    /// @notice The hashes contract used by the this redemption contract.
+    IHashes public constant HASHES =
+        IHashes(address(0xD07e72b00431af84AD438CA995Fd9a7F0207542d));
 
     /// @notice The HashesDAO contract that the ETH in the redemption will be
     ///         returned to after the deadline has passed.
-    IHashesDAO public immutable hashesDAO;
+    IHashesDAO public constant HASHES_DAO =
+        IHashesDAO(address(0xbD3Af18e0b7ebB30d49B253Ab00788b92604552C));
 
     /// @notice The deadline to redeem a hash. Redemptions are considered
     ///         finalized after this deadline and funds can be moved back into
@@ -41,45 +43,53 @@ contract Redemption is ReentrancyGuard {
     ///         back to the HashesDAO.
     bool public wasDrawn;
 
+    /// @notice Thrown when the duration is invalid.
+    error InvalidDuration();
+
+    /// @notice Thrown when the current timestamp is after the deadline but the
+    ///         function can only be called before the deadline.
+    error AfterDeadline();
+
+    /// @notice Thrown when the current timestamp is before the deadline but the
+    ///         function can only be called after the deadline.
+    error BeforeDeadline();
+
+    /// @notice Thrown when the token IDs aren't monotonically increasing.
+    error UnsortedTokenIds();
+
+    /// @notice Thrown when the hash isn't eligible for redemption.
+    error IneligibleHash();
+
+    /// @notice Thrown when a hash wasn't committed by the sender or wasn't
+    ///         committed at all.
+    error UncommittedHash();
+
+    /// @notice Thrown when ether can't be transferred.
+    error TransferFailed();
+
     /// @notice Instantiates the Hashes redemption contract.
-    /// @param _hashes The Hashes token.
-    /// @param _hashesDAO The HashesDAO.
     /// @param _duration The duration of the redemption.
-    constructor(IHashes _hashes, IHashesDAO _hashesDAO, uint256 _duration) {
-        // Ensure that the HashesDAO address is non-zero.
-        require(
-            address(_hashesDAO) != address(0),
-            "Redemption: Invalid HashesDAO."
-        );
-
-        // Ensure that the Hashes token is tracked by governance.
-        require(
-            _hashesDAO.hashesToken() == _hashes,
-            "Redemption: Invalid Hashes token."
-        );
-
+    constructor(uint256 _duration) {
         // Ensure that the duration is greater than zero.
-        require(_duration > 0, "Redemption: Invalid duration.");
+        if (_duration == 0) {
+            revert InvalidDuration();
+        }
 
-        // Initialize the immutables.
-        hashes = _hashes;
-        hashesDAO = _hashesDAO;
+        // Initialize the deadline.
         deadline = block.timestamp + _duration;
     }
 
     modifier beforeDeadline() {
-        require(
-            block.timestamp < deadline,
-            "Redemption: The deadline has passed."
-        );
+        if (block.timestamp >= deadline) {
+            revert AfterDeadline();
+        }
         _;
     }
 
     modifier afterDeadline() {
-        require(
-            block.timestamp >= deadline,
-            "Redemption: The deadline hasn't passed yet."
-        );
+        if (block.timestamp < deadline) {
+            revert BeforeDeadline();
+        }
         _;
     }
 
@@ -98,27 +108,27 @@ contract Redemption is ReentrancyGuard {
         // hold, we take custody of the Hashes token and add the token to the
         // list of commitments.
         uint256 lastTokenId;
-        uint256 governanceCap = hashes.governanceCap();
+        uint256 governanceCap = HASHES.governanceCap();
         for (uint256 i = 0; i < _tokenIds.length; i++) {
             // Ensure that the current token ID is either the first token ID to
             // be processed or is strictly greater than the last token ID.
-            require(
-                i == 0 || _tokenIds[i] > lastTokenId,
-                "Redemption: The token IDs aren't monotonically increasing."
-            );
+            if (i != 0 && _tokenIds[i] <= lastTokenId) {
+                revert UnsortedTokenIds();
+            }
             lastTokenId = _tokenIds[i];
 
             // Ensure that the token ID refers to an active DAO hash that is
             // not a DEX Labs Hash.
-            require(
-                lastTokenId >= DEX_LABS_CAP &&
-                    lastTokenId < governanceCap &&
-                    !hashes.deactivated(lastTokenId),
-                "Redemption: Token ID refers to a non-DAO hash, a deactivated DAO hash, or a DEX Labs hash."
-            );
+            if (
+                lastTokenId < DEX_LABS_CAP ||
+                lastTokenId >= governanceCap ||
+                HASHES.deactivated(lastTokenId)
+            ) {
+                revert IneligibleHash();
+            }
 
             // Take custody of the Hashes token.
-            hashes.safeTransferFrom(msg.sender, address(this), lastTokenId);
+            HASHES.safeTransferFrom(msg.sender, address(this), lastTokenId);
 
             // Process the commitment.
             totalCommitments += 1;
@@ -140,25 +150,23 @@ contract Redemption is ReentrancyGuard {
         for (uint256 i = 0; i < _tokenIds.length; i++) {
             // Ensure that the current token ID is either the first token ID to
             // be processed or is strictly greater than the last token ID.
-            require(
-                i == 0 || _tokenIds[i] > lastTokenId,
-                "Redemption: The token IDs aren't monotonically increasing."
-            );
+            if (i != 0 && _tokenIds[i] <= lastTokenId) {
+                revert UnsortedTokenIds();
+            }
             lastTokenId = _tokenIds[i];
 
             // Ensure that the token ID refers to a commitment owned by the
             // sender.
-            require(
-                commitments[lastTokenId] == msg.sender,
-                "Redemption: Token ID doesn't refer to a commitment."
-            );
+            if (commitments[lastTokenId] != msg.sender) {
+                revert UncommittedHash();
+            }
 
             // Revoke the commitment.
             totalCommitments -= 1;
             commitments[lastTokenId] = address(0);
 
             // Transfer the Hash to the sender.
-            hashes.transferFrom(address(this), msg.sender, lastTokenId);
+            HASHES.transferFrom(address(this), msg.sender, lastTokenId);
         }
     }
 
@@ -174,18 +182,16 @@ contract Redemption is ReentrancyGuard {
         for (uint256 i = 0; i < _tokenIds.length; i++) {
             // Ensure that the current token ID is either the first token ID to
             // be processed or is strictly greater than the last token ID.
-            require(
-                i == 0 || _tokenIds[i] > lastTokenId,
-                "Redemption: The token IDs aren't monotonically increasing."
-            );
+            if (i != 0 && _tokenIds[i] <= lastTokenId) {
+                revert UnsortedTokenIds();
+            }
             lastTokenId = _tokenIds[i];
 
             // Ensure that the token ID refers to a commitment owned by the
             // sender.
-            require(
-                commitments[lastTokenId] == msg.sender,
-                "Redemption: Token ID doesn't refer to a commitment."
-            );
+            if (commitments[lastTokenId] != msg.sender) {
+                revert UncommittedHash();
+            }
 
             // Reset the commitment to prevent double spends.
             commitments[lastTokenId] = address(0);
@@ -205,7 +211,9 @@ contract Redemption is ReentrancyGuard {
             _tokenIds.length * 1 ether
         );
         (bool success, ) = msg.sender.call{value: redeemAmount}("");
-        require(success, "Redemption: Transfer failed.");
+        if (!success) {
+            revert TransferFailed();
+        }
     }
 
     /// @notice Allows anyone to transfer any ether that isn't needed to process
@@ -234,10 +242,12 @@ contract Redemption is ReentrancyGuard {
             wasDrawn = true;
 
             // Transfer the unused funds to the HashesDAO.
-            (bool success, ) = address(hashesDAO).call{
+            (bool success, ) = address(HASHES_DAO).call{
                 value: totalFunding - redeemFunds
             }("");
-            require(success, "Redemption: Transfer failed.");
+            if (!success) {
+                revert TransferFailed();
+            }
         }
     }
 
@@ -254,14 +264,17 @@ contract Redemption is ReentrancyGuard {
         for (uint256 i = 0; i < _tokenIds.length; i++) {
             // Ensure that the current token ID is either the first token ID to
             // be processed or is strictly greater than the last token ID.
-            require(
-                i == 0 || _tokenIds[i] > lastTokenId,
-                "Redemption: The token IDs aren't monotonically increasing."
-            );
+            if (i != 0 && _tokenIds[i] <= lastTokenId) {
+                revert UnsortedTokenIds();
+            }
             lastTokenId = _tokenIds[i];
 
             // Transfer the Hash to the HashesDAO.
-            hashes.transferFrom(address(this), address(hashesDAO), lastTokenId);
+            HASHES.transferFrom(
+                address(this),
+                address(HASHES_DAO),
+                lastTokenId
+            );
         }
     }
 
